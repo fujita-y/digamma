47b18c9af745f158278dfa66358f6f6151e8a519
Yoshikatsu Fujita <y.fujita.lwp@gmail.com>
2019-12-29 17:31:30 -0800

    finish cond-expand

--- stdlib/scheme/base.scm
+++ stdlib/scheme/base.scm
@@ -1,3 +1,6 @@
+;;; Copyright (c) 2004-2019 Yoshikatsu Fujita / LittleWing Company Limited.
+;;; See LICENSE file for terms and conditions of use.
+
 (define-library
   (scheme base)
   (import (except (core) for-each map)
