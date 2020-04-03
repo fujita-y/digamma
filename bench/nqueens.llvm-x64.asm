95ba64fd68a8fc24080bb063976e00b02c91c9a7
fujita-y <y.fujita.lwp@gmail.com>
2020-04-03 00:04:33 -0700

    add digamma ir sample

--- bench/nqueens.llvm-x64.asm
+++ bench/nqueens.llvm-x64.asm
@@ -1,4 +1,4 @@
-  .section	__TEXT,__text,regular,pure_instructions
+	.section	__TEXT,__text,regular,pure_instructions
 	.macosx_version_min 10, 15
 	.intel_syntax noprefix
 	.globl	"_27571fde-30ff-4ccb-96d7-18ad93155dfd" ## -- Begin function 27571fde-30ff-4ccb-96d7-18ad93155dfd
