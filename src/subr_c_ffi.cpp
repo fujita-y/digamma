58cccd77bd2c208ec58d3cd634cbe65e32b3c7ec
fujita-y <y.fujita.lwp@gmail.com>
2022-01-04 22:01:58 +0000

    Update copyright year

--- src/subr_c_ffi.cpp
+++ src/subr_c_ffi.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2004-2022 Yoshikatsu Fujita / LittleWing Company Limited.
+// See LICENSE file for terms and conditions of use.
+
 #include "core.h"
 #include "vm.h"
 #include "file.h"
