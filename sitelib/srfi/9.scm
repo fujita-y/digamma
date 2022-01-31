64205f58d45c412ad4fafbdcc15bcc548b91acd8
fujita-y <y.fujita.lwp@gmail.com>
2022-01-30 20:46:39 -0800

    fix records

--- sitelib/srfi/9.scm
+++ sitelib/srfi/9.scm
@@ -5,14 +5,29 @@
   (begin
     (define-syntax define-record-type/srfi-9
       (lambda (x)
-        (define parse
-          (lambda (stx)
-            (syntax-case stx
-              ()
+        (define parse-spec
+          (lambda (spec)
+            (syntax-case spec ()
               ((x y) (syntax (immutable x y)))
               ((x y z) (syntax (mutable x y z))))))
-        (syntax-case x
-          ()
-          ((_ type (ctor _ ...) pred spec ...)
-           (with-syntax (((spec ...) (map parse (syntax (spec ...)))))
-             (syntax (define-record-type (type ctor pred) (fields spec ...))))))))))
+        (syntax-case x ()
+          ((_ type (ctor constructor-tag ...) pred spec ...)
+           (with-syntax ((((field-tag _ ...) ...)
+                          (syntax (spec ...)))
+                         ((r6rs:field-spec ...)
+                          (map parse-spec (syntax (spec ...)))))
+             (let ((ctags (syntax (constructor-tag ...))))
+               (with-syntax (((constructor-arg ...)
+                              (map (lambda (ftag)
+                                     (if (find (lambda (e) (bound-identifier=? e ftag)) ctags)
+                                         ftag
+                                         (syntax (unspecified))))
+                                   (syntax (field-tag ...)))))
+                 (syntax
+                   (define-record-type
+                     (type ctor pred)
+                     (protocol
+                       (lambda (ctor)
+                         (lambda (constructor-tag ...)
+                           (ctor constructor-arg ...))))
+                     (fields r6rs:field-spec ...))))))))))))
