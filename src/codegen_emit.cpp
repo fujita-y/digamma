2f2bd486f4751cab126868e6a278b90e7d65e163
fujita-y <y.fujita.lwp@gmail.com>
2026-03-07 19:35:35 -0800

    set helper no inline

--- src/codegen_emit.cpp
+++ src/codegen_emit.cpp
@@ -309,6 +309,8 @@ llvm::Function* codegen_t::get_or_create_call_closure_bridge() {
     // Symbol exists in JIT, just provide external declaration in this module
     auto f2 = llvm::Function::Create(ft, llvm::Function::ExternalLinkage, name, main_module);
     f2->setDSOLocal(true);
+    f2->removeFnAttr(llvm::Attribute::AlwaysInline);
+    f2->addFnAttr(llvm::Attribute::NoInline);
     return f2;
   } else {
     // Consume the error (symbol not found)
@@ -317,6 +319,8 @@ llvm::Function* codegen_t::get_or_create_call_closure_bridge() {
 
   f = llvm::Function::Create(ft, llvm::Function::ExternalLinkage, name, main_module);
   f->setDSOLocal(true);
+  f->removeFnAttr(llvm::Attribute::AlwaysInline);
+  f->addFnAttr(llvm::Attribute::NoInline);
 
   llvm::BasicBlock* saved_block = BL.GetInsertBlock();
   llvm::BasicBlock* entry = llvm::BasicBlock::Create(CT, "entry", f);
