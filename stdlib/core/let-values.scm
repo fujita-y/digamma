e8bf09e291ce45071306da7c53a2a33b2629031c
fujita-y <y.fujita.lwp@gmail.com>
2026-05-27 18:54:23 -0700

    fix let-values

--- stdlib/core/let-values.scm
+++ stdlib/core/let-values.scm
@@ -4,22 +4,40 @@
 (define-module (core let-values)
   (export let-values let*-values)
   
+  ;; srfi-11 reference implementation
   (define-syntax let-values
     (syntax-rules ()
-      ((let-values () body1 body2 ...)
-       (let () body1 body2 ...))
-      ((let-values (((v ...) expr) rest ...) body1 body2 ...)
-       (call-with-values
-         (lambda () expr)
-         (lambda (v ...)
-           (let-values (rest ...) body1 body2 ...))))))
+      ((let-values (?binding ...) ?body0 ?body1 ...)
+      (let-values "bind" (?binding ...) () (begin ?body0 ?body1 ...)))
+      
+      ((let-values "bind" () ?tmps ?body)
+      (let ?tmps ?body))
+      
+      ((let-values "bind" ((?b0 ?e0) ?binding ...) ?tmps ?body)
+      (let-values "mktmp" ?b0 ?e0 () (?binding ...) ?tmps ?body))
+      
+      ((let-values "mktmp" () ?e0 ?args ?bindings ?tmps ?body)
+      (call-with-values 
+        (lambda () ?e0)
+        (lambda ?args
+          (let-values "bind" ?bindings ?tmps ?body))))
+      
+      ((let-values "mktmp" (?a . ?b) ?e0 (?arg ...) ?bindings (?tmp ...) ?body)
+      (let-values "mktmp" ?b ?e0 (?arg ... x) ?bindings (?tmp ... (?a x)) ?body))
+      
+      ((let-values "mktmp" ?a ?e0 (?arg ...) ?bindings (?tmp ...) ?body)
+      (call-with-values
+        (lambda () ?e0)
+        (lambda (?arg ... . x)
+          (let-values "bind" ?bindings (?tmp ... (?a x)) ?body))))))
 
   (define-syntax let*-values
     (syntax-rules ()
-      ((let*-values () body1 body2 ...)
-       (let () body1 body2 ...))
-      ((let*-values (binding rest ...) body1 body2 ...)
-       (let-values (binding)
-         (let*-values (rest ...) body1 body2 ...)))))
+      ((let*-values () ?body0 ?body1 ...)
+      (begin ?body0 ?body1 ...))
 
+      ((let*-values (?binding0 ?binding1 ...) ?body0 ?body1 ...)
+      (let-values (?binding0)
+        (let*-values (?binding1 ...) ?body0 ?body1 ...)))))
+        
 ) ;[end]
